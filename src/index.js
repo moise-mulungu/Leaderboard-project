65d7aa588b7ef8ba2dbc1c46bb3d12244f2109f6
moise <moisemlg90@gmail.com>
2022-03-17 15:03:53 +0200

    implement API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
+import getData from '.modules/connect.js';
 import './style.css';
+
+const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api//games/Zl4d7IVkemOTTVg2fUdz/scores/';
+const formInput = document.getElementById('input');
+const nameInput =  document.getElementById('name');
+const scoreInput =  document.getElementById('score');
+const refreshBtn = document.getElementById('refreshButton');
+
+formInput.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const addName = nameInput.value;
+    const addScores = scoreInput.value;
+    fetch (url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+
+        },
+        body: JSON.stringify({
+            addName,
+            addScores,
+        }),
+    });
+    nameInput.value = '';
+    scoreInput.value = '';
+})
+refreshBtn.addEventListener('click', getData);
+getData();
